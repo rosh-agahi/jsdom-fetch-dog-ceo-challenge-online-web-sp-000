176f0a8abf5927c6f4cdf191d4242171f887b04f
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 00:27:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ console.log('%c HI', 'color: firebrick')
 const dogBreeds = document.querySelector("#dog-breeds")
 const breedDropdown = document.querySelector("#breed-dropdown")
 
+document.addEventListener('DOMContentLoaded', function() {
+  loadImages();
+  loadDogBreeds();
+  addBreedSelectListener()
+})
+
+function loadImages() {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(results => { results.message.forEach(image => addImage(image))});
+}
