a634a22d5fa0926a034806ed65fdfbb58d46ee09
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 00:28:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -14,3 +14,28 @@ function loadImages() {
     .then(res => res.json())
     .then(results => { results.message.forEach(image => addImage(image))});
 }
+
+function addImage(image) {
+  let container = document.getElementById("dog-image-container");
+  let newImg = document.createElement("img");
+  newImg.src = image;
+  container.appendChild(newImg);
+}
+
+let breeds = []
+function loadDogBreeds() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then(results => {
+      breeds = Object.keys(results.message);
+      breeds.forEach(breed => addBreed(breed))});
+}
+
+function addBreed(breed) {
+  let breedUl = document.getElementById("dog-breeds");
+  let newLi = document.createElement("li");
+  newLi.innerText = breed;
+  newLi.style.cursor = "pointer";
+  breedUl.appendChild(newLi);
+  newLi.addEventListener("click", changeColor)
+}
